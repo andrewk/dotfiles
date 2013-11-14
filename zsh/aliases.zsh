1fd081d69898af58faaa09b1a1f41a4d2ee5ba27
Andrew Krespanis <andrew@leftjustified.net>
2013-11-14 11:51:29 +1100

    git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,19 +1,24 @@
 alias pstree="pstree -g 2"
 alias hd="hexdump -C"
 
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
 # git
 alias gs='git status'
 alias gd='git diff -M'
 alias gf='git fetch'
+alias glog='git log --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset"'
 gls() {
   query="$1"
   shift
-  glog --pickaxe-regex "-S$query" "$@"
+  git log --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset" --pickaxe-regex "-S$query" "$@"
 }
-alias glog='git log --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset"'
+alias glsearch='gls'
 alias gl='glog --graph'
 alias gla='gl --all'
 alias glp='glog --patch'
+alias glmerge='gla --simplify-by-decoration'
 
 # rake
 alias rake='noglob rake'
